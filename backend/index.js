bc7fce693edae596c3627233062176ca57686de8
Vaishnavi Ghiradkar <20bcs088@iiitdmj.ac.in>
2023-06-24 23:07:47 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose 
+ .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,   })   
+ .then(() => console.log("MongoDB connected!"))
+ .catch(err => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static("client/build"));
+}
+
+app.listen(process.env.PORT||8800 , () => {
+  console.log("Backend server is running!");
+});
